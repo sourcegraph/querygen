8264c6753b6628ef2c785e37ff43f38d0dd569e1
Varun Gandhi <varun.gandhi@sourcegraph.com>
2024-06-09 14:06:31 +0800

    fix: Use 'Var' instead of 'Params' suffix (#4)
    In a parameterized statement, 'parameters' refers to
    the placeholders in the statement, whereas 'variables'
    refer to the values passed alongside the statement.

--- lib/interpolate/interpolate.go
+++ lib/interpolate/interpolate.go
@@ -1,12 +1,13 @@
 package interpolate
 
 import (
-	"github.com/keegancsmith/sqlf"
+	"fmt"
 
+	"github.com/keegancsmith/sqlf"
 	"github.com/sourcegraph/querygen/internal"
 )
 
-type QueryParams interface {
+type QueryVars interface {
 	// FormatSpecifiers returns 1 element per interpolation
 	FormatSpecifiers() []string
 	// FormatArgs returns 1 element per interpolation
@@ -21,7 +22,10 @@ func (e *QueryDoesntUseInterpolationError) Error() string {
 	return "query doesn't use interpolation"
 }
 
-func Do(query string, q QueryParams) (*sqlf.Query, error) {
+// Do creates a sqlf.Query from the given query string and QueryVars.
+//
+// If the query doesn't use interpolation, returns nil, &QueryDoesntUseInterpolationError{}.
+func Do(query string, q QueryVars) (*sqlf.Query, error) {
 	formatSpecs := q.FormatSpecifiers()
 	matchIndex := 0
 	modifiedQuery := internal.SubstitutionRegex.ReplaceAllStringFunc(query, func(match string) string {
@@ -31,6 +35,24 @@ func Do(query string, q QueryParams) (*sqlf.Query, error) {
 	})
 	if matchIndex == 0 {
 		return nil, &QueryDoesntUseInterpolationError{}
+	}
+	return sqlf.Sprintf(modifiedQuery, q.FormatArgs()...), nil
+}
+
+// MustDo creates a sqlf.Query from the given query string and QueryVars.
+//
+// Panics if the query doesn't use interpolation.
+func MustDo(query string, q QueryVars) (*sqlf.Query, error) {
+	formatSpecs := q.FormatSpecifiers()
+	matchIndex := 0
+	modifiedQuery := internal.SubstitutionRegex.ReplaceAllStringFunc(query, func(match string) string {
+		replacement := formatSpecs[matchIndex]
+		matchIndex += 1
+		return replacement
+	})
+	if matchIndex == 0 {
+		err := &QueryDoesntUseInterpolationError{}
+		panic(fmt.Sprintf("%s: %25s", err.Error(), query))
 	}
 	return sqlf.Sprintf(modifiedQuery, q.FormatArgs()...), nil
 }
